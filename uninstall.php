d9a69c7fd4106bce3a9c4e7313eb2534781e81d1
Arifur Rahman Arif <dragonwarrior052@gmail.com>
2022-01-27 17:30:19 +0600

    added uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,27 @@
+<?php
+
+defined('WP_UNINSTALL_PLUGIN') || wp_die(__('You can\'t access this page', 'wsmgs'));
+
+class WooCommerceStockManagementWithSheet {
+    public function __construct() {
+        $this->deleteOptions();
+    }
+
+    public function deleteOptions() {
+        $savedOptions = [
+            'wsmgsToken',
+            'sheetUrl',
+            'tabName'
+        ];
+
+        foreach ($savedOptions as $option) {
+            delete_option($option);
+        }
+    }
+}
+
+if (!class_exists('WooCommerceStockManagementWithSheet')) {
+    return;
+}
+
+new WooCommerceStockManagementWithSheet();
